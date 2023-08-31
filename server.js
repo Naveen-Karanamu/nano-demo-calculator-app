14d5e4b2f60faf41de546503758715a8acaad27e
Naveen Kumar <karanamunaveen2002@gmail.com>
2023-08-31 16:11:17 +0530

    Debug : Crash

--- server.js
+++ server.js
@@ -9,22 +9,36 @@ app.use(express.json());
 
 const baseRouter = express.Router();
 
-baseRouter.get("/greeting", (req, res) => {
-  try {
-    return res.json({ code: 200, content: "Hello World!" });
-  } catch (error) {
-    return res.json({ code: 500, Error: error });
-  }
+// baseRouter.get("/greeting", (req, res) => {
+//   try {
+//     return res.json({ code: 200, content: "Hello World!" });
+//   } catch (error) {
+//     return res.json({ code: 500, Error: error });
+//   }
+// });
+
+// baseRouter.post("/add", (req, res) => {
+//   const sum = req.body.first + req.body.second;
+//   res.json({ result: sum });
+// });
+
+// baseRouter.post("/subtract", (req, res) => {
+//   const sub = req.body.first - req.body.second;
+//   res.json({ result: sub });
+// });
+baseRouter.get('/greeting', (req, res) => {
+    return res.send('Hello World!');
 });
 
-baseRouter.post("/add", (req, res) => {
-  const sum = req.body.first + req.body.second;
-  res.json({ result: sum });
+baseRouter.post('/add', (req, res) => {
+    const { first, second } = req.body;
+    res.json({ "result": first + second });
 });
 
-baseRouter.post("/subtract", (req, res) => {
-  const sub = req.body.first - req.body.second;
-  res.json({ result: sub });
+
+baseRouter.post('/subtract', (req, res) => {
+    const { first, second } = req.body;
+    res.json({ "result": first - second });
 });
 
 app.use(baseUrl, baseRouter);
