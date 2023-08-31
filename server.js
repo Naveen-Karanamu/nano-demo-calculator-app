90806fd2856eff185c1e7d413c5f3dd403bd93a7
Naveen Kumar <karanamunaveen2002@gmail.com>
2023-08-31 15:47:34 +0530

    Check Actions

--- server.js
+++ server.js
@@ -1,35 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = "/calculator";
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
-baseRouter.get('/greeting', (req, res) => {
-    try{
-        return res.json({code:200,content:"Hello World!"});
-    }catch(error){
-        return res.json({code:500, Error:error})
-    }
+baseRouter.get("/greeting", (req, res) => {
+  try {
+    return res.json({ code: 200, content: "Hello World!" });
+  } catch (error) {
+    return res.json({ code: 500, Error: error });
+  }
 });
 
-baseRouter.post('/add', (req, res) => {
-    const sum=req.body.first+req.body.second;
-    res.json({ result: sum });
-    
+baseRouter.post("/add", (req, res) => {
+  const sum = req.body.first + req.body.second;
+  res.json({ result: sum });
 });
 
-
-baseRouter.post('/subtract', (req, res) => {
-    const sub=req.body.first-req.body.second;
-    res.json({ result: sub });
+baseRouter.post("/subtract", (req, res) => {
+  const sub = req.body.first - req.body.second;
+  res.json({ result: sub });
 });
 
 app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
+  console.log("Server running at PORT", PORT);
 });
